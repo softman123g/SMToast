7b9498c3648eb0e43871829810f74f5b36d79cd8
softman123g <softman123g@126.com>
2016-03-02 12:12:52 +0800

    finished SMToast

--- SMToast/SMToast.swift
+++ SMToast/SMToast.swift
@@ -1,65 +1,131 @@
 //
 //  SMToast.swift
-//  TakePictureAndRecordVideo
 //
 //  Created by softman on 16/1/29.
 //  Copyright © 2016年 softman. All rights reserved.
 //  一个Toast显示工具
+//  A simple Toast notification, like Android Toast. Used Swift.
+//
+//  1.支持显示多行文本
+//  2.支持设置显示时长
+//  3.支持距顶部、底部的距离
+//  4.支持点击Toast隐藏
+//  5.支持屏幕旋转时自动隐藏
+//  6.支持回车换行
+//  7.静态方法直接调用
 
 
 import UIKit
 
 class SMToast: NSObject {
     private static let DEFAULT_DURATION = 2.0
-    private var text:NSString = ""
-    private var duration = 2.0
-    private var contentView:UIButton!
     
-    class func showText(text:String, duration:Double){
-        let toast = SMToast(text: text, duration: duration)
-        toast.showToast()
-    }
     class func showText(text:String){
-        let toast = SMToast(text: text, duration: DEFAULT_DURATION)
+        let toast = SMToast()
+        toast.text = text
         toast.showToast()
     }
+    class func showText(text:String, duration:Double){
+        let toast = SMToast()
+        toast.text = text
+        toast.duration = duration
+        toast.showToast()
+    }
+    class func showText(text:String, topOffset:CGFloat){
+        let toast = SMToast()
+        toast.text = text
+        toast.topOffset = topOffset
+        toast.showToast()
+    }
+    class func showText(text:String, topOffset:CGFloat, duration:Double){
+        let toast = SMToast()
+        toast.text = text
+        toast.topOffset = topOffset
+        toast.duration = duration
+        toast.showToast()
+    }
+    class func showText(text:String, bottomOffset:CGFloat){
+        let toast = SMToast()
+        toast.text = text
+        toast.bottomOffset = bottomOffset
+        toast.showToast()
+    }
+    class func showText(text:String, bottomOffset:CGFloat, duration:Double){
+        let toast = SMToast()
+        toast.text = text
+        toast.bottomOffset = bottomOffset
+        toast.duration = duration
+        toast.showToast()
+    }
+    
+    private var text:NSString = "" {
+        didSet {
+            let font = UIFont.boldSystemFontOfSize(14)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineBreakMode = .ByWordWrapping
+            let attrDict = [NSFontAttributeName: font, NSParagraphStyleAttributeName: paragraphStyle]
+            let expectedRect = text.boundingRectWithSize(CGSizeMake(280,CGFloat(MAXFLOAT)), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attrDict, context: nil)
+            let textSize = expectedRect.size
+            
+            textLabel.frame = CGRect(x: 0, y: 0, width: textSize.width + 12, height: textSize.height + 12)
+            textLabel.backgroundColor = UIColor.clearColor()
+            textLabel.textColor = UIColor.whiteColor()
+            textLabel.textAlignment = .Center
+            textLabel.font = font
+            textLabel.text = text as String
+            textLabel.numberOfLines = 0
+            
+            contentView.frame = CGRectMake(0,0,textLabel.frame.size.width, textLabel.frame.size.height)
+            contentView.layer.cornerRadius = 5.0
+            contentView.layer.borderWidth = 1.0
+            contentView.layer.borderColor = UIColor.grayColor().colorWithAlphaComponent(0.5).CGColor
+            contentView.backgroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.75)
+            contentView.addSubview(textLabel)
+            contentView.autoresizingMask = .FlexibleWidth
+            contentView.addTarget(self, action: "toastTapped:", forControlEvents: .TouchDown)
+            contentView.alpha = 0.0
+            if let _window = UIApplication.sharedApplication().keyWindow {
+                contentView.center = _window.center
+            }
+        }
+    }
+    private var duration = SMToast.DEFAULT_DURATION
+    private var textLabel:UILabel = UILabel()
+    private var contentView:UIButton = UIButton()
+    private var topOffset:CGFloat = 0 {
+        didSet{
+            let window = UIApplication.sharedApplication().keyWindow
+            if let _window = window {
+                contentView.center = CGPointMake(_window.center.x, topOffset + self.contentView.frame.size.height/2)
+            }
+        }
+    }
+    private var bottomOffset:CGFloat = 0 {
+        didSet {
+            let window = UIApplication.sharedApplication().keyWindow
+            if let _window = window {
+                contentView.center = CGPointMake(_window.center.x, _window.frame.size.height - (bottomOffset + self.contentView.frame.size.height/2))
+            }
+        }
+    }
+    
     private override init() {
         super.init()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "deviceOrientationDidChanged:", name: UIDeviceOrientationDidChangeNotification, object: UIDevice.currentDevice())
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIDeviceOrientationDidChangeNotification, object: UIDevice.currentDevice())
     }
 
-    private init(text:NSString, duration:Double) {
-        super.init()
-        self.text = text
-        self.duration = duration
-        let font = UIFont.boldSystemFontOfSize(14)
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineBreakMode = .ByWordWrapping
-        let attrDict = [NSFontAttributeName: font, NSParagraphStyleAttributeName: paragraphStyle]
-        let expectedRect = text.boundingRectWithSize(CGSizeMake(280,CGFloat(MAXFLOAT)), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attrDict, context: nil)
-        let textSize = expectedRect.size
-        let textLabel:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: textSize.width + 12, height: textSize.height + 12))
-        textLabel.backgroundColor = UIColor.clearColor()
-        textLabel.textColor = UIColor.whiteColor()
-        textLabel.textAlignment = .Center
-        textLabel.font = font
-        textLabel.text = text as String
-        textLabel.numberOfLines = 0
-        
-        contentView = UIButton(frame: CGRectMake(0,0,textLabel.frame.size.width, textLabel.frame.size.height))
-        contentView.layer.cornerRadius = 5.0
-        contentView.layer.borderWidth = 1.0
-        contentView.layer.borderColor = UIColor.grayColor().colorWithAlphaComponent(0.5).CGColor
-        contentView.backgroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.75)
-        contentView.addSubview(textLabel)
-        contentView.autoresizingMask = .FlexibleWidth
-        contentView.addTarget(self, action: "toastTapped:", forControlEvents: .TouchDown)
-        contentView.alpha = 0.0
+    func deviceOrientationDidChanged(notify: NSNotification){
+        self.hideAnimation()
     }
     private func showAnimation(){
         UIView.beginAnimations("show", context: nil)
         UIView.setAnimationCurve(.EaseIn)
         UIView.setAnimationDuration(0.3)
-        contentView?.alpha = 1.0
+        contentView.alpha = 1.0
         UIView.commitAnimations()
     }
     func hideAnimation(){//selector不能够是private的
@@ -68,7 +134,7 @@ class SMToast: NSObject {
         UIView.setAnimationDelegate(self)
         UIView.setAnimationDidStopSelector("dismissToast")
         UIView.setAnimationDuration(0.3)
-        contentView?.alpha = 1.0
+        contentView.alpha = 0.0
         UIView.commitAnimations()
     }
     func dismissToast(){
@@ -77,11 +143,11 @@ class SMToast: NSObject {
     func toastTapped(sender:UIButton){
         self.hideAnimation()
     }
+    
     private func showToast(){
         let window = UIApplication.sharedApplication().keyWindow
         if let _window = window {
-            contentView.center = _window.center
-            window?.addSubview(contentView)
+            _window.addSubview(contentView)
             self.showAnimation()
             self.performSelector("hideAnimation", withObject: nil, afterDelay: self.duration)
         }
--- SMToastDemo/ViewController.swift
+++ SMToastDemo/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton()
+        button.frame = CGRectMake(50, 120, 150, 50)
+        button.backgroundColor = UIColor.redColor()
+        button.setTitle("Show Demo Text", forState: .Normal)
+        button.addTarget(self, action: "showDemo:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func showDemo(sender:UIButton) {
+        sender.alpha = 0.0
+        //        SMToast.showText("居中显示")
+        SMToast.showText("居中显示", duration: 5)
+//        SMToast.showText("距顶部50像素", topOffset: 50)
+        SMToast.showText("距顶部50像素", topOffset: 50, duration: 5)
+        SMToast.showText("支持设定显示时间", topOffset: 100, duration: 5)
+        SMToast.showText("文字很多时，将根据文字的内容自动多行显示。比如说这里将使用2行显示。", topOffset: 150, duration: 5)
+        SMToast.showText("支持\'\\n\'，可\n显示\n多\n行", bottomOffset: 100, duration: 5)
+        SMToast.showText("支持屏幕旋转时自动消失", bottomOffset: 40, duration: 5)
+//        SMToast.showText("距底部5像素", bottomOffset: 5)
+        SMToast.showText("距底部5像素", bottomOffset: 5, duration: 5)
+    }
 }
-
